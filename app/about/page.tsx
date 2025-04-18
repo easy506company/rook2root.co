2ff498feca7d38bee4d18a0ec36bc985b58f4c70
Wiktor Żołnierowicz <w.zolnierowicz@gmail.com>
2025-04-18 13:25:10 +0200

    analytics & small css adjustments

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,9 @@ export default function About() {
     return (
         <div className="min-h-[87vh] px-2 sm:py-28 py-36 flex flex-col gap-4 items-center">
             <div className="text-center flex flex-col items-center justify-center w-fit gap-2">
-                <h2 className="text-7xl font-bold pr-1">rook2root</h2>
+                <h1 className="text-[1.80rem] leading-8 sm:px-8 md:leading-[4.5rem] font-bold mb-4 sm:text-6xl text-left sm:text-center">
+                    rook2root
+                </h1>
                 <p className="text-muted-foreground text-md font-medium">
                     business is business
                 </p>
