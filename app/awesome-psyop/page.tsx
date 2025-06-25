7e9be00e07c68c94635999560da21c5f206c26ea
gdanska mewa <admin@rook2root.co>
2025-06-25 09:43:45 +0200

    dunamic markdown loading

--- app/awesome-psyop/page.tsx
+++ app/awesome-psyop/page.tsx
@@ -1,10 +1,20 @@
 'use client';
-import Content from './awesome-psyops-temp.mdx';
+
+import { useEffect, useState } from 'react';
+import { marked } from 'marked';
 
 export default function Page() {
+  const [markdown, setMarkdown] = useState('');
+
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/easy506company/awesome-psyop/refs/heads/main/README.mdx')
+      .then((res) => res.text())
+      .then((text) => setMarkdown(text));
+  }, []);
+
   return (
     <div className="prose dark:prose-invert max-w-4xl mx-auto py-12">
-      <Content />
+      <div dangerouslySetInnerHTML={{ __html: marked.parse(markdown) }} />
     </div>
   );
 }
